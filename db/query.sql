ef45f249ccd1df0b0d780fa564326e473eef7f92
Sheri Brown <sherib70@gmail.com>
2022-06-10 19:30:48 -0400

    updated seeds.sql

--- db/query.sql
+++ db/query.sql
@@ -1,15 +1,15 @@
 SELECT *
 FROM department_names;
 
-SELECT department, COUNT(id) AS number_courses
-FROM course_names
+SELECT  AS 
+FROM department_names
 GROUP BY department;
 
-SELECT department, 
-FROM course_names
+SELECT role, 
+FROM department_names
 GROUP BY department;
 
 SELECT
-  favorite_books.book_name AS name, book_prices.price AS price
-FROM favorite_books
-JOIN book_prices ON favorite_books.book_price = book_prices.id;
+   AS name,  AS 
+FROM 
+JOIN department ON department_names.department = department.id;
