4e7c083c26ccc4a6097403631909bf0bea3e1c38
Sheri Brown <sherib70@gmail.com>
2022-06-10 18:15:37 -0400

    updated seeds.sql data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,29 @@
+USE employees_db;
+
 INSERT INTO departments (department_name)
-VALUES  ("manager"),
-        ("engineer"),
-        ("sales"),
-        (""),
-        ("");
+VALUES  
+    ("Engineer"),
+    ("Sales"),
+    ("Finance"),
+    ("Human Resources")
+    ("Legal");
+    ("Information Technology"),
 
 
-INSERT INTO roles (department_id, role)
-VALUES (1, ""),
+INSERT INTO role (title, salary, department_id,)
+VALUES 
+    ("Accountant", 65000, 1),
+    ("Legal Team", 85000, 2),
+    ("Lawyer", 120000, 3),
+    ("Sales Lead", 55000, 4),
+    ("Salesperson", 40000, 5),
+    ("Lead Engineer", 110000, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Megan Brown"),
+("Sara Cox"),
+("Sam Cash"),
+("Jon Doe"),
+("Mike Warne"),
+("Ashley Saxon");
