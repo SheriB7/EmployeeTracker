0439b59c9f72f6ba173a8a0e0f1fd26a749cf9c5
Sheri Brown <sherib70@gmail.com>
2022-06-14 23:35:55 -0400

    worked on choices for What would you like to do. Fixed wrong spelling and errors in if statement. ShowDepartments and Show roles completed and working. Add role code is written and working. Add department is up and running.

--- index.js
+++ index.js
@@ -68,107 +68,100 @@ const promptStart = async () => {
           "View ALL Departments",
           "View All Roles?",
           "View All Employees?",
-          "View All Employees By Manager",
-          "Add an Employee?",
+          // "View All Employees By Manager",
+          // "Add an Employee?",
           "Add a Role",
-          "Add a Department?",
-          "Update Employee"
+          "Add a department",
+          // "Update Employee",
+          "No Action"
         ]
       }
     ])
-  //     //using the value and
-  //     .then((value) => {
-  //       console.log(value)
-  // })
-
-  // promptStart()
 
 
-
-
-  // .then((data) => {
   const { choices } = data;
-  if (choices === "View all departments") {
-    showDepartments();
+  // console.log(choices)
+  if (choices === "View ALL Departments") {
+    // console.log(choices)
+    return showDepartments();
   }
-  if (choices === "View all roles") {
-    showRoles();
+  if (choices === "View All Roles?") {
+    return showRoles();
   }
-  if (choices === "View all employees") {
-    showEmployes();
+  if (choices === "View All Employees?") {
+    return showEmployees();
   }
   if (choices === "Add a department") {
-    showDepartment();
+    return addDepartment();
   }
-  if (choices === "Add a role") {
-    showRole();
+  if (choices === "Add a Role") {
+    return addRole();
   }
-  if (choices === "Add an employee") {
-    showEmployee();
+  if (choices === "Add an Employee?") {
+    return addEmployee();
   }
-  if (choices === "Update an employee role") {
-    updateEmployee();
+  if (choices === "Update Employee") {
+    return updateEmployee();
   }
   if (choices === "Update an employee managers") {
-    updateManager();
+    return updateManager();
   }
   if (choices === "View employees by manager") {
-    employeeDepartment();
+    return employeeDepartment();
   }
   if (choices === "Delete a department") {
-    deleteDepartment();
+    return deleteDepartment();
   }
   if (choices === "Delete a role") {
-    deleteRole();
+    return deleteRole();
   }
   if (choices === "Delete an employee") {
-    deleteEmployee();
+    return deleteEmployee();
   }
   if (choices === "View department budgets") {
-    viewBudget();
+    return viewBudget();
   }
   if (choices === "No Action") {
-    connection.end()
+    return db.end()
   };
   //});
 }
-showDepartments = () => {
-  console.log('Showing all departments...\n');
-  const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+// showDepartments = () => {
+//   console.log('Showing all departments...\n');
+//   const sql = `SELECT department.id AS id, department.name AS department FROM department`;
 
-  connection.promise().query(sql, (err, rows) => {
-    if (err) throw err;
-    console.table(rows);
-    promptStart();
-  });
-};
+//   connection.promise().query(sql, (err, rows) => {
+//     if (err) throw err;
+//     console.table(rows);
+//     promptStart();
+//   });
+// };
 // promptStart()
 
 const showDepartments = async () => {
   try {
     const sql = `SELECT department.id AS id, department.name AS department FROM department`;
-    const response = await query('sql;')
+    const response = await query(sql)
     console.table(response)
+    promptStart()
   } catch (error) {
     console.error(error)
   }
 }
 
 //Show all Roles
-showRoles = () => {
+const showRoles = async () => {
   console.log("Showing all roles...\n");
 
-  const sql = "Select role.id, role.title, department_name AS department FROM role INNER JOIN department ON role.department_id = deparment.id";
+  const sql = "Select role.id, role.title, department.name AS department FROM role INNER JOIN department ON role.department_id = department.id";
 
-  connection.promise().query(sql, (err, rows) => {
-    if (err) throw err;
-    console.table(rows);
-    promptStart();
-  })
+  const response = await query(sql)
+  console.table(response)
+  promptStart()
 };
 
 // Show all Employees
-showEmployees = () => {
+const showEmployees = async () => {
   console.log('Showing all employees...\n');
   const sql = `SELECT employee.id, 
                       employee.first_name, 
@@ -182,17 +175,67 @@ showEmployees = () => {
                       LEFT JOIN department ON role.department_id = department.id
                       LEFT JOIN employee manager ON employee.manager_id = manager.id`;
 
-  connection.promise().query(sql, (err, rows) => {
-    if (err) throw err;
-    console.table(rows);
-    promptUser();
-  });
+  const response = await query(sql)
+  console.table(response)
+  promptStart()
 };
 
 
 // add Department
 
 
+const addDepartment = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "addDept",
+      message: "What department name?",
+      // validate: addDept => {
+      //   if (addDept) {
+      //     return true;
+      //   } else {
+      //     console.log("Enter a department");
+      //     return false;
+      //   }
+      // }
+    }
+  ])
+  const sql = "INSERT INTO department (name) VALUES (?)"
+  await query(sql, answers.addDept)
+  showDepartments()
+}
+
+// add Role
+const addRole = async () => {
+  const sql1 = `SELECT department.id AS value, department.name AS name FROM department`;
+    const choices = await query(sql1)
+    //console.log(response)
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Role name"
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "Enter salary"
+    },
+    {
+      type: "list",
+      name: "department_id",
+      message: "Which department will this role be assigned to?",
+      choices: choices
+    },
+
+  ])
+  // console.log(answers)
+  const sql2 = "INSERT INTO role (title, salary, department_id) VALUES(?,?,?)"
+  await query(sql2, [answers.title, answers.salary, answers.department_id])
+  showRoles()
+}
+
+promptStart()
 
 
 
